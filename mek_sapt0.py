b5ba69175cd356e39450622f504e4a7719158d08
ALescoulie <alescoul@calpoly.edu>
2022-01-07 10:30:39 -0800

    write python script

--- mek_sapt0.py
+++ mek_sapt0.py
@@ -1,2 +1,14 @@
-from MDSAPT.mdsapt import Reader, Optimizer, TrajectorySAPT
+from os import path
+import sys
 
+from MDSAPT.mdsapt import Reader, Optimizer, TrajectorySAPT
+from MDSAPT.mdsapt.reader import InputReader
+
+if __name__ == '__main__':
+    Settings = InputReader(sys.argv[1])
+
+    Opt = Optimizer(Settings)
+
+    SAPTRun = TrajectorySAPT(Settings, Optimizer).run(Settings.start, Settings.stop, Settings.step)
+
+    SAPTRun.results.to_csv(sys.argv[2])
