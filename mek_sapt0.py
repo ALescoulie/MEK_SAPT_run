9f762e5f43ad5900e2317effed6fb3358614d5e3
alescoulie <alia.lescoulie@gmail.com>
2022-01-12 23:08:07 -0800

    update imports

--- mek_sapt0.py
+++ mek_sapt0.py
@@ -1,14 +1,13 @@
 from os import path
 import sys
 
-from MDSAPT.mdsapt import Reader, Optimizer, TrajectorySAPT
-from MDSAPT.mdsapt.reader import InputReader
+from mdsapt import InputReader, Optimizer, TrajectorySAPT
 
 if __name__ == '__main__':
-    Settings = InputReader(sys.argv[1])
+    Settings = InputReader('atp_glu_leu_met.yaml')
 
     Opt = Optimizer(Settings)
 
     SAPTRun = TrajectorySAPT(Settings, Optimizer).run(Settings.start, Settings.stop, Settings.step)
 
-    SAPTRun.results.to_csv(sys.argv[2])
+    SAPTRun.results.to_csv('results.csv')
